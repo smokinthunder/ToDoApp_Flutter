953e0a0c15b706f7f9e94f5a7cd89882d18735b8
Antesh <anteshkumar2003@gmail.com>
2023-09-23 12:28:45 +0530

    Initial Commit

---
+++ lib/screens/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:todos_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title:  Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          const Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          SizedBox(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          )
+        ],),
+      ),
+    );
+  }
+}
